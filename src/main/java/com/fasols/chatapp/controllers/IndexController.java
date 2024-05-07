dcd6542902b0c3a5f46c088158fc620ebfb0e89e
faizykhalid <faizankhalid1636@gmail.com>
2024-05-08 03:24:44 +0500

    First commit

---
+++ src/main/java/com/fasols/chatapp/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.fasols.chatapp.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class IndexController {
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getHome(){
+        return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping(path = "index",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getIndex(){
+        return this.getHome();
+    }
+}
